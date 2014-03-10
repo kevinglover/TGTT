5bb1ed10511364963d68866e24751774cbc91399
kgnow <hkglover@gmail.com>
2014-03-10 00:56:37 -0400

    Update to authors page
    Edited the positioning of images and text

--- tgtt-roots/templates/content-authors.php
+++ tgtt-roots/templates/content-authors.php
@@ -16,12 +16,12 @@
               $html .= "<div class='row'>";
 
               
-              $html .= "<div class='col-xs-2 col-sm-2'>";
+              $html .= "<div class='col-xs-3 col-md-2'>";
               $alt = get_the_author_meta('firstname',$userID) . ' '.get_the_author_meta('lastname',$userID);
               $html .= '<a href="'.get_author_posts_url($userID).'" class="avatar-link">';
               $html .= get_avatar( $userID, $size, $default, $alt );
               $html .= "</a></div>";
-              $html .= "<div class='col-xs-8 col-sm-10'>";
+              $html .= "<div class='col-xs-8 col-sm-9'>";
               $html .= '<h2><a href="'.get_author_posts_url($userID).'">' . $user->user_firstname . ' ' . $user->user_lastname . '</a></h2>';
               $html .= "</div>";
               $html .= "</div>";
