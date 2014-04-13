8e14e75a0681083be6c13c78d230db53ef90b0c8
Kristian Serrano <kristianserrano@gmail.com>
2014-04-13 12:15:38 -0400

    Sorted Authors page
    Sorted authors page by number of posts. Blog owners would likely have
    more posts than guests. Also employed get_users() in lieu of deprecated
    get_users_of_blog(), and used values in returned array to reduce calls
    to other functions. Lastly, made use of display name rather than
    concatenating first name and last name. display name should be used for
    display.

--- tgtt-roots/templates/content-authors.php
+++ tgtt-roots/templates/content-authors.php
@@ -2,27 +2,31 @@
   <div class="entry-content">
       <?php
         //displays all users ($users will also contain all usermeta fields for each user
-        $blogusers = get_users_of_blog();
+        $blogusers = get_users('orderby=post_count&order=DESC');
         $html .= '<dl>'; 
 
         if ($blogusers) {
           foreach ($blogusers as $bloguser) {
-            $user = get_userdata($bloguser->user_id);
-            $userID=$user->ID;
+            $userID = $bloguser->ID;
+            //echo $userID;
+            //$userID=$user->ID;
             $post_count = get_usernumposts($userID);
             if ($post_count) {
+            //echo $post_count.',';
               
               $html .= '<dt>'; 
               $html .= "<div class='row'>";
 
               
               $html .= "<div class='col-xs-3 col-md-2'>";
-              $alt = get_the_author_meta('firstname',$userID) . ' '.get_the_author_meta('lastname',$userID);
+              $alt = $bloguser->display_name;
+                            //$alt = get_the_author_meta('firstname',$userID) . ' '.get_the_author_meta('lastname',$userID);
               $html .= '<a href="'.get_author_posts_url($userID).'" class="avatar-link">';
               $html .= get_avatar( $userID, $size, $default, $alt );
               $html .= "</a></div>";
               $html .= "<div class='col-xs-8 col-sm-9'>";
-              $html .= '<h2><a href="'.get_author_posts_url($userID).'">' . $user->user_firstname . ' ' . $user->user_lastname . '</a></h2>';
+              //$html .= '<h2><a href="'.get_author_posts_url($userID).'">' . $user->user_firstname . ' ' . $user->user_lastname . '</a></h2>';
+              $html .= '<h2><a href="'.get_author_posts_url($userID).'">' . $alt . '</a></h2>';
               $html .= "</div>";
               $html .= "</div>";
 
